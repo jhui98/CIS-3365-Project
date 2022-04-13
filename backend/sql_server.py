7fa3fdb6399a97911612317158667f7cb050118f
jhui98 <jacob.hui@live.com>
2022-04-12 23:59:06 -0500

    trying

--- backend/sql_server.py
+++ backend/sql_server.py
@@ -1,3 +1,5 @@
+import flask 
+from flask import jsonify
 import pyodbc
 conn = pyodbc.connect('Driver={SQL Server}; Server=172.26.54.46,1433;Database=Triple Take Solutions Project;UID=Triple;PWD=Triple12;Trusted_connection=no;')
 print(conn)
@@ -8,11 +10,14 @@ cursor = conn.cursor()
 #     print(i)
 
 def execute_query(connection, query):
+    values = []
     cursor = connection.cursor()
     cursor.execute(query)
 
     for i in cursor:
-        print(i)
+        values.append(i)
+        # print(i)
+    print(jsonify(values))
 
 query = 'SELECT * FROM dbo.[item]'
 execute_query(conn, query)
